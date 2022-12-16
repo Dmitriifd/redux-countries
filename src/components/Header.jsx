c642f419c3bd8587054a07f76c91e245d21e08cb
Dmitriifd <idimka9@gmail.com>
2022-12-16 17:49:04 +0500

    очистка фильтров

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import { IoMoon, IoMoonOutline } from 'react-icons/io5'
 import { setTheme } from 'store/theme/theme-actions'
 
 import { Container } from './Container'
-
+import { clearControls } from 'store/controls/controls-actions'
 
 const HeaderEl = styled.header`
 	box-shadow: var(--shadow);
@@ -43,6 +43,8 @@ export const Header = () => {
 
 	const toggleTheme = () => dispatch(setTheme(theme === 'light' ? 'dark' : 'light'))
 
+	const cleanUp = () => dispatch(clearControls())
+
 	useEffect(() => {
 		document.body.setAttribute('data-theme', theme)
 	}, [theme])
@@ -51,7 +53,7 @@ export const Header = () => {
 		<HeaderEl>
 			<Container>
 				<Wrapper>
-					<Title>Where is the world?</Title>
+					<Title onClick={cleanUp}>Where is the world?</Title>
 					<ModeSwitcher onClick={toggleTheme}>
 						{theme === 'light' ? <IoMoonOutline size="14px" /> : <IoMoon size="14px" />}{' '}
 						<span style={{ marginLeft: '0.75rem' }}>{theme} Theme</span>
