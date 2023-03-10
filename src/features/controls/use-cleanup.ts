fdf856ca01203e5fbd13d401a928ad88b4915c55
Dmitriifd <idimka9@gmail.com>
2023-03-10 20:03:34 +0500

    типизация controls

---
+++ src/features/controls/use-cleanup.ts
@@ -0,0 +1,8 @@
+import { useAppDispatch } from 'store';
+import { clearControls } from './controls-slice';
+
+export const useCleanup = () => {
+  const dispatch = useAppDispatch();
+  const cleanUp = () => dispatch(clearControls());
+  return cleanUp;
+};
